e093a1d74c2d264751fcbc9ced9e338b915dc0e0
hit <90evs@gmail.com>
2020-11-18 22:45:22 +0900

    First commit for glitch test

---
+++ vacard3_server.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+  var stream = fs.createReadStream('vacard3.html');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  stream.pipe(res);
+});
+var io = require('socket.io')(server);
+server.listen(8000);
+
+io.sockets.on('connection', function(socket) {
+    console.log('connect')
+    socket.on('client_img_click', function(img_coord){
+        console.log('client_img_click', img_coord);
+        io.emit('server_img_click', img_coord);
+    });
+    socket.on('debug_print', function(data){
+        console.log('debug_print', data);
+    });
+    
+  });
